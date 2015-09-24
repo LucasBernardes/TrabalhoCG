fec213efc85188ea79e2f829bdc1b05c93dfc95d
dreymaior <andrey.maior@gmail.com>
2015-09-24 15:33:22 -0300

    New version - GraphWin crack

--- danteTrab.py
+++ danteTrab.py
@@ -1,10 +1,11 @@
 import pygame
 import calculos
+import math
+
 from pygame.locals import *
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
-mat = [[2.6,0,0],[0,1.26,0],[0,0,1]]
 
 
 def lerArq(nome):
@@ -26,32 +27,29 @@ def lerArq(nome):
     y = 0
     tam = (len(vertice[0]))
 
-    '''m = calculos.mMatrizes(mat,aresta)
-    
     for x in range(0,tam):
-        glVertex3fv((m[0][vertice[y][x]],m[1][vertice[y][x]],m[2][vertice[y][x]]))
-        glVertex3fv((m[0][vertice[y+1][x]],m[1][vertice[y+1][x]],m[2][vertice[y+1][x]]))
-
-    '''
-
-    for x in range(0,tam):
-        glVertex3fv((aresta[0][vertice[y][x]],aresta[1][vertice[y][x]],aresta[2][vertice[y][x]]))
-        glVertex3fv((aresta[0][vertice[y+1][x]],aresta[1][vertice[y+1][x]],aresta[2][vertice[y+1][x]]))
-
+        glVertex3fv((aresta[0][vertice[y][x]], aresta[1][vertice[y][x]],\
+                aresta[2][vertice[y][x]]))  
+        glVertex3fv((aresta[0][vertice[y+1][x]], aresta[1][vertice[y+1][x]],\
+                aresta[2][vertice[y+1][x]]))
     glEnd()
-
-
-
-    
 
 def main():
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    print(display[0]/display[1])
-    gluPerspective(50, float(display[0])/float(display[1]), 0.1, 50.0)
+    DEGRAD = 3.14159265/180
+    tangent = math.tan(50/2 * DEGRAD)
+    height = 8.0 * tangent
+    width = height * float(display[0])/float(display[1])
+
+    #gluPerspective(50, float(display[0])/float(display[1]), 8.0, 50.0)
+
+    glFrustum(-width, width, -height, height, 7.0, 50.0)
+    #glOrtho(-width, width, -height, height, 7.0, 50.0)
 
     glTranslatef(-1.0,-1.0,-10.0)
+    #glTranslatef(-2.5,-2.5,-10.0)     # Ponto de vista A, B, C
     nomearq = "cubo.txt"
     '''raw_input('Inserir nome do arq: ')'''
     while True:
@@ -60,7 +58,7 @@ def main():
                 pygame.quit()
                 quit()
 
-        glRotatef(0, 0, 0, 0)
+        #glRotatef(0.5, 1, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         '''Aqui chama o arquivo '''
         
