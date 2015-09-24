243fae557e0b617781640da2bcc5e0a1b90b63f0
LucasBernardes <lukazbernardez@gmail.com>
2015-09-24 18:14:31 -0300

    lerArq, fazGrafico
    feito

--- danteTrab.py
+++ danteTrab.py
@@ -3,53 +3,43 @@ from pygame.locals import *
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
-matrixVert =[
-    [0,2,0,2,0,2,0,2],
-    [0,0,2,2,0,0,2,2],
-    [0,0,0,0,2,2,2,2]
-]
 
-matrixEdge = [
-    [0,0,0,1,2,2,3,1,4,4,5,6],
-    [1,2,4,3,3,6,7,5,6,5,7,7]
-]
-
-edges = (
-    (0,1),
-    (0,2),
-    (0,4),
-    (1,3),
-    (2,3),
-    (2,6),
-    (3,7),
-    (1,5),
-    (4,6),
-    (4,5),
-    (5,7),
-    (6,7)
-    )
-
-
-def Cube():
+def lerArq(nome):
+    linestring = open(nome, 'r').read()
+    a = linestring.splitlines()
+    hasht = 0
+    vertice = []
+    aresta = []
+    for x in range (0,len(a)):
+      if a[x] == "#":
+        hasht = 1
+      elif (hasht == 1):
+            arestaaux = [int(s) for s in a[x].split(",") if s.isdigit()]
+            vertice.append(arestaaux)
+      else:
+            vertaux = [int(s) for s in a[x].split(",") if s.isdigit()]
+            aresta.append(vertaux)
     glBegin(GL_LINES)
     y = 0
-    tam = (len(edges))
+    tam = (len(vertice[0]))
     for x in range(0,tam):
-        glVertex3fv((matrixVert[0][matrixEdge[y][x]],matrixVert[1][matrixEdge[y][x]],matrixVert[2][matrixEdge[y][x]]))
-        glVertex3fv((matrixVert[0][matrixEdge[y+1][x]],matrixVert[1][matrixEdge[y+1][x]],matrixVert[2][matrixEdge[y+1][x]]))
+        glVertex3fv((aresta[0][vertice[y][x]],aresta[1][vertice[y][x]],aresta[2][vertice[y][x]]))
+        glVertex3fv((aresta[0][vertice[y+1][x]],aresta[1][vertice[y+1][x]],aresta[2][vertice[y+1][x]]))
     glEnd()
+
+
 
     
 
 def main():
     pygame.init()
-    display = (800,800)
+    display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
     gluPerspective(50, (display[0]/display[1]), 0.1, 50.0)
 
-    glTranslatef(-1.0,-1.0,-5.0)
-
+    glTranslatef(-1.0,-1.0,-10.0)
+    nomearq = raw_input('Inserir nome do arq: ')
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -58,7 +48,10 @@ def main():
 
         glRotatef(0, 0, 0, 0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        Cube()
+        '''Aqui chama o arquivo '''
+        
+        
+        lerArq(str(nomearq))
         pygame.display.flip()
         pygame.time.wait(10)
 
